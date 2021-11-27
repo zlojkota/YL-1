b977f8bbeaf934c4717555edd8f6be5d9bd2ffa5
i.kotiv@big3.ru <i.kotiv@big3.ru>
2021-11-27 13:16:49 +0300

    Fix routers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,6 +12,9 @@ func GaugeRoute(w http.ResponseWriter, r *http.Request) {
 	//gaugeRoute := []string{"Alloc", "BuckHashSys", "Frees", "GCCPUFraction", "GCSys", "HeapAlloc", "HeapIdle", "HeapInuse", "HeapObjects", "HeapReleased", "HeapSys", "LastGC", "Lookups", "MCacheInuse", "MCacheSys", "MSpanInuse", "MSpanSys", "Mallocs", "NextGC", "NumForcedGC", "NumGC", "OtherSys", "PauseTotalNs", "StackInuse", "StackSys", "Sys", "RandomValue"}
 	uri, _ := url.Parse(r.URL.RequestURI())
 	realPath := strings.Replace(uri.Path, "/update/gauge", "", 1)
+	if string([]rune(realPath)[0]) == "/" {
+		realPath = strings.Replace(realPath, "/", "", 1)
+	}
 	all := strings.Split(realPath, "/")
 	log.Printf("Method: %s, URI: %s", r.Method, all)
 	if len(all) == 2 {
@@ -29,10 +32,13 @@ func GaugeRoute(w http.ResponseWriter, r *http.Request) {
 func CounterRoute(w http.ResponseWriter, r *http.Request) {
 	uri, _ := url.Parse(r.URL.RequestURI())
 	realPath := strings.Replace(uri.Path, "/update/counter", "", 1)
+	if string([]rune(realPath)[0]) == "/" {
+		realPath = strings.Replace(realPath, "/", "", 1)
+	}
 	all := strings.Split(realPath, "/")
 	log.Printf("Method: %s, URI: %s", r.Method, all)
 	if len(all) == 2 {
-		_, err := strconv.ParseFloat(all[1], 64)
+		_, err := strconv.ParseInt(all[1], 0, 64)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 		} else {
