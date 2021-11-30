2cd167257f82fef4e7f8b30d91b6b687cb1d94ed
i.kotiv@big3.ru <i.kotiv@big3.ru>
2021-11-30 08:54:10 +0300

    Fix Errors in test

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -19,66 +19,66 @@ type Collector struct {
 	Done     <-chan struct{}
 }
 
-func (m *Collector) collect() {
+func (this *Collector) collect() {
 
 	var rtm runtime.MemStats
 	runtime.ReadMemStats(&rtm)
 
-	m.gauge["Alloc"] = float64(rtm.Alloc)
-	m.gauge["BuckHashSys"] = float64(rtm.BuckHashSys)
-	m.gauge["Frees"] = float64(rtm.Frees)
-	m.gauge["GCCPUFraction"] = float64(rtm.GCCPUFraction)
-	m.gauge["GCSys"] = float64(rtm.GCSys)
-	m.gauge["HeapAlloc"] = float64(rtm.HeapAlloc)
-	m.gauge["HeapIdle"] = float64(rtm.HeapIdle)
-	m.gauge["HeapInuse"] = float64(rtm.HeapInuse)
-	m.gauge["HeapObjects"] = float64(rtm.HeapObjects)
-	m.gauge["HeapReleased"] = float64(rtm.HeapReleased)
-	m.gauge["HeapSys"] = float64(rtm.HeapSys)
-	m.gauge["LastGC"] = float64(rtm.LastGC)
-	m.gauge["Lookups"] = float64(rtm.Lookups)
-	m.gauge["MCacheInuse"] = float64(rtm.MCacheInuse)
-	m.gauge["MCacheSys"] = float64(rtm.MCacheSys)
-	m.gauge["MSpanInuse"] = float64(rtm.MSpanInuse)
-	m.gauge["MSpanSys"] = float64(rtm.MSpanSys)
-	m.gauge["Mallocs"] = float64(rtm.Mallocs)
-	m.gauge["NextGC"] = float64(rtm.NextGC)
-	m.gauge["NumForcedGC"] = float64(rtm.NumForcedGC)
-	m.gauge["NumGC"] = float64(rtm.NumGC)
-	m.gauge["OtherSys"] = float64(rtm.OtherSys)
-	m.gauge["PauseTotalNs"] = float64(rtm.PauseTotalNs)
-	m.gauge["StackInuse"] = float64(rtm.StackInuse)
-	m.gauge["StackSys"] = float64(rtm.StackSys)
-	m.gauge["Sys"] = float64(rtm.Sys)
-	m.counter["PollCount"]++
-	m.gauge["RandomValue"] = float64(rand.Float32())
+	this.gauge["Alloc"] = float64(rtm.Alloc)
+	this.gauge["BuckHashSys"] = float64(rtm.BuckHashSys)
+	this.gauge["Frees"] = float64(rtm.Frees)
+	this.gauge["GCCPUFraction"] = float64(rtm.GCCPUFraction)
+	this.gauge["GCSys"] = float64(rtm.GCSys)
+	this.gauge["HeapAlloc"] = float64(rtm.HeapAlloc)
+	this.gauge["HeapIdle"] = float64(rtm.HeapIdle)
+	this.gauge["HeapInuse"] = float64(rtm.HeapInuse)
+	this.gauge["HeapObjects"] = float64(rtm.HeapObjects)
+	this.gauge["HeapReleased"] = float64(rtm.HeapReleased)
+	this.gauge["HeapSys"] = float64(rtm.HeapSys)
+	this.gauge["LastGC"] = float64(rtm.LastGC)
+	this.gauge["Lookups"] = float64(rtm.Lookups)
+	this.gauge["MCacheInuse"] = float64(rtm.MCacheInuse)
+	this.gauge["MCacheSys"] = float64(rtm.MCacheSys)
+	this.gauge["MSpanInuse"] = float64(rtm.MSpanInuse)
+	this.gauge["MSpanSys"] = float64(rtm.MSpanSys)
+	this.gauge["Mallocs"] = float64(rtm.Mallocs)
+	this.gauge["NextGC"] = float64(rtm.NextGC)
+	this.gauge["NumForcedGC"] = float64(rtm.NumForcedGC)
+	this.gauge["NumGC"] = float64(rtm.NumGC)
+	this.gauge["OtherSys"] = float64(rtm.OtherSys)
+	this.gauge["PauseTotalNs"] = float64(rtm.PauseTotalNs)
+	this.gauge["StackInuse"] = float64(rtm.StackInuse)
+	this.gauge["StackSys"] = float64(rtm.StackSys)
+	this.gauge["Sys"] = float64(rtm.Sys)
+	this.counter["PollCount"]++
+	this.gauge["RandomValue"] = float64(rand.Float32())
 
 }
 
-func (p *Collector) Handle(duration time.Duration, handle CollectorHandle) {
-	p.duration = duration
-	p.handle = handle
+func (this *Collector) Handle(duration time.Duration, handle CollectorHandle) {
+	this.duration = duration
+	this.handle = handle
 }
 
-func (p *Collector) Run() {
-	p.gauge = map[string]float64{}
-	p.counter = map[string]int64{}
-	p.collect()
-	if p.duration == 0 {
-		p.duration = time.Second
+func (this *Collector) Run() {
+	this.gauge = map[string]float64{}
+	this.counter = map[string]int64{}
+	this.collect()
+	if this.duration == 0 {
+		this.duration = time.Second
 	}
-	tick := time.NewTicker(p.duration)
+	tick := time.NewTicker(this.duration)
 	defer tick.Stop()
 	for {
 		select {
-		case <-p.Done:
+		case <-this.Done:
 			return
 		case <-tick.C:
-			p.collect()
-			if p.handle == nil {
-				fmt.Print(p)
+			this.collect()
+			if this.handle == nil {
+				fmt.Print(this)
 			} else {
-				p.handle.HandleData(p.counter, p.gauge)
+				this.handle.HandleData(this.counter, this.gauge)
 			}
 		}
 	}
