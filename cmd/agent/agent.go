9c88bc381d667dbda9e80d790bddef9f1877fba3
i.kotiv@big3.ru <i.kotiv@big3.ru>
2021-11-30 09:08:52 +0300

    Fix Errors in test ------------------- Close body in req

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -18,14 +18,22 @@ func (p *Agent) agentInit(duration time.Duration) {
 func (p *Agent) HandleData(counter map[string]int64, gauge map[string]float64) {
 	for key, val := range gauge {
 		strval := fmt.Sprintf("%f", val)
-		_, err := http.Post("http://localhost:8080/update/gauge/"+string(key)+"/"+strval, "text/plain", nil)
+		res, err := http.Post("http://localhost:8080/update/gauge/"+string(key)+"/"+strval, "text/plain", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = res.Body.Close()
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
 	for key, val := range counter {
 		strval := fmt.Sprintf("%d", val)
-		_, err := http.Post("http://localhost:8080/update/counter/"+string(key)+"/"+strval, "text/plain", nil)
+		res, err := http.Post("http://localhost:8080/update/counter/"+string(key)+"/"+strval, "text/plain", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = res.Body.Close()
 		if err != nil {
 			fmt.Println(err)
 		}
