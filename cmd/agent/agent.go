2c764051da6f3b5d60242f54a0bf72f960c8d7a9
i.kotiv@big3.ru <i.kotiv@big3.ru>
2021-12-15 07:51:13 +0300

    inc5 --------------- add env

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -7,12 +7,16 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/labstack/gommon/log"
 	"github.com/zlojkota/YL-1/internal/agentcollector"
 	"github.com/zlojkota/YL-1/internal/collector"
 )
 
 type Worker struct {
+	ServerAddr     string `env:"ADDRESS"`
+	ReportInterval int64  `env:"REPORT_INTERVAL"`
+	PoolInterval   int64  `env:"POLL_INTERVAL"`
 }
 
 func (p *Worker) RequestServe(req *http.Request) {
@@ -31,8 +35,15 @@ func main() {
 	var t collector.Collector
 	var agent agentcollector.Agent
 	var worker Worker
-	agent.InitAgent(&worker)
-	t.Handle(2*time.Second, &agent)
+	err := env.Parse(&worker)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if worker.PoolInterval == 0 {
+		worker.PoolInterval = 2
+	}
+	agent.InitAgent(&worker, worker.ServerAddr)
+	t.Handle(time.Duration(worker.PoolInterval)*time.Second, &agent)
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan,
